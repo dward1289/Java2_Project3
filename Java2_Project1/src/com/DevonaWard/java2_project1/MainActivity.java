4a123610dc5aa059efe8f0e15c14f34f41716834
devona <dward1289@fullsail.edu>
2013-09-17 21:05:45 -0400

    Finally working UI and Spinner, Reading and displaying data from storage

---
+++ Java2_Project1/src/com/DevonaWard/java2_project1/MainActivity.java
@@ -0,0 +1,90 @@
+package com.DevonaWard.java2_project1;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+	Context context = this;
+	JSONArray theSavedObject;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	//On click for the "Go" button
+	@SuppressLint("HandlerLeak")
+	public void startService(View view) {
+		
+		//HANDLER GOES HERE
+		Handler theHandler = new Handler(){
+			public void handleMessage(Message msg){
+				super.handleMessage(msg);
+			}
+			
+		};
+		
+		Messenger dataMessenger = new Messenger(theHandler);
+		Intent intent = new Intent(this, theService.class);
+		intent.putExtra("messenger", dataMessenger);
+		startService(intent);
+
+		showData();
+		
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	
+	private void showData(){
+		//Data pulled
+		String savedData = SingletonClass.readStringFile(context, "teamsJSONString", false);
+		//Spinner gets position
+		Spinner spinner = (Spinner) findViewById(R.id.team_spinner);
+		int spinnerPosition = spinner.getSelectedItemPosition();
+		//Text views to hold called data
+		TextView teamN = (TextView) findViewById(R.id.teamTxt);
+		TextView teamAb = (TextView) findViewById(R.id.abTxt);
+		TextView teamAr = (TextView) findViewById(R.id.arenaTxt);
+		TextView teamDiv = (TextView) findViewById(R.id.divTxt);
+		
+			try {
+				theSavedObject = new JSONArray (savedData);
+				JSONObject mainObj = theSavedObject.getJSONObject(spinnerPosition);
+				//Data put in to strings
+				String fullName = mainObj.getString("full_name");
+				String abbreviation = mainObj.getString("abbreviation");
+				String areaName = mainObj.getString("site_name");
+				String division = mainObj.getString("division");
+				//Datta displayed in the text views.
+				teamN.setText(fullName);
+				teamAb.setText(abbreviation);
+				teamAr.setText(areaName);
+				teamDiv.setText(division);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	}
+}
